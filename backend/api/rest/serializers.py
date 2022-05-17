981ee42cf9d6dbfcecbeb77c304ef5c65c82bda9
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-05-17 22:06:48 +0200

    Add some documentation

--- backend/api/rest/serializers.py
+++ backend/api/rest/serializers.py
@@ -4,11 +4,17 @@ from rest_framework import serializers
 # from rest_framework import permissions
 
 class UserSerializer(serializers.ModelSerializer):
+    '''
+    Main User model serializer
+    '''
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'groups', 'is_superuser', 'is_active']
 
 class GroupSerializer(serializers.ModelSerializer):
+    '''
+    Main Group model serializer
+    '''
     class Meta:
         model = Group
         fields = ['id', 'name']
